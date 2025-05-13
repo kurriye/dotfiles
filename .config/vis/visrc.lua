30fe62c25e1b6d78b5dae8f45fbaee14260b09a0
kurriye <kurriye@tuta.io>
2025-05-13 13:08:25 +0000

    Create visrc.lua

---
+++ .config/vis/visrc.lua
@@ -0,0 +1,15 @@
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+	-- Your global configuration options
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
+	-- Your per window configuration options e.g.
+	-- vis:command('set number')
+	vis:command('set autoindent on')
+	vis:command('set number on')
+	vis:command('set cul on')
+
+end)
