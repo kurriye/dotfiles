b51b2f4a5cb86cd6cfbc9330889db117a5a65a14
kurriye <kurriye@tuta.io>
2026-07-18 04:52:10 -0300

    updated vis config

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,15 +1,13 @@
--- load standard vis module, providing parts of the Lua API
 require('vis')
 
 vis.events.subscribe(vis.events.INIT, function()
-	-- Your global configuration options
+        -- global config
 end)
 
-vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
-	-- Your per window configuration options e.g.
-	-- vis:command('set number')
-	vis:command('set autoindent on')
-	vis:command('set number on')
-	vis:command('set cul on')
-
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+        vis:command('set autoindent on')
+        vis:command('set number on')
+        vis:command('set cul on')
+        vis:command('set tabwidth 4')
+        vis:command('set expand on')
 end)
